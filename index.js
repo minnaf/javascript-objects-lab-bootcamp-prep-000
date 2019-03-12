d8c2fe597a44d0cf52ad4de17bc77ada2b901ad7
Minna Fingerhood <minnaf@sas.upenn.edu>
2019-03-12 01:27:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key){
-  delete object.key;
-  return object
+  var newObject = Object.assign({}, object);
+  delete newObject.key; 
+  return newObject
 }
